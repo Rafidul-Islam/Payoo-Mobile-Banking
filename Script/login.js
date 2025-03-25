bea15e26c13a0b98fbcf5d07819bc9662b753e75
Rafidul-Islam <rafid.iconic@gmail.com>
2025-03-26 00:16:28 +0600

    made the login section more functional and secured

--- Script/login.js
+++ Script/login.js
@@ -1,13 +1,34 @@
 const loginBtn = document.getElementById("login-btn");
-
+const enter = document.getElementById("pin-number");
 loginBtn.addEventListener("click", function () {
   const accountNumber = document.getElementById("account-number").value;
   const pinNumber = document.getElementById("pin-number").value;
   if (accountNumber == "01748609307" && pinNumber == "6576") {
     window.location.href = "main.html";
   } else if (accountNumber === "" && pinNumber === "") {
-    alert("Please Enter Account Number & PIN");
+    alert("Please enter Account Number And PIN Number");
+  } else if (accountNumber === "") {
+    alert("Please Enter Account Number");
+  } else if (pinNumber === "") {
+    alert("Please Enter PIN Number");
   } else {
     alert("Wrong Credentials !!");
+  }
+});
+enter.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    if (accountNumber == "01748609307" && pinNumber == "6576") {
+      window.location.href = "main.html";
+    } else if (accountNumber === "" && pinNumber === "") {
+      alert("Please enter Account Number And PIN Number");
+    } else if (accountNumber === "") {
+      alert("Please Enter Account Number");
+    } else if (pinNumber === "") {
+      alert("Please Enter PIN Number");
+    } else {
+      alert("Wrong Credentials !!");
+    }
   }
 });
